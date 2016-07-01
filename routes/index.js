c52127812015b80c23cf60773fa1c30d943506c7
marcioheleno <marcio@marcioheleno.website>
2016-06-30 21:25:59 -0300

    ajustes para fins

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('layout', { title: '' });
+  res.render('layout', { title: 'Controler de Lista' });
 });
 
 module.exports = router;
